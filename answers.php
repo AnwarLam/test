2d517ff85037b9a0aa3ae2d9786c41d4ac9d882b
AnwarLam <anouar.lemkarane@gmail.com>
2016-03-11 20:49:17 +0100

    Update answers.php

--- answers.php
+++ answers.php
@@ -1,1 +1,42 @@
+question A : 1) 10
+
+question B : 5) aucune des reponses ci-dessus
+
+question C : 1) file
+
+question D : 3) une erreur fatale
+
+question E : 4) krsort
+
+question F : 3) 110
+
+question G: 2) non
+
+question H: reponse 2) et 3)
+
+question I: 2) 20
+
+question J: 2) 1
+
+question K: 3) 1234
+
+question L: 1) 2
+
+question M: 4) trigger('cliquer')
+
+question N:
+<script type="text/javascript">
+function returnContentMyDiv()
+{
+	return $('.myDiv').html();
+}
+$(document).ready(function(){
+	
+	returnContentMyDiv();
+});
+</script> 
+
+
+
+
 
