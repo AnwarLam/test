53aaa1caf95d0511ab3a890e2a64cdf61b9dce24
AnwarLam <anouar.lemkarane@gmail.com>
2016-03-11 20:11:28 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,1 +1,14 @@
+<?php 
 
+//initialisation de la reponse
+$response = "no";
+
+//verification de l'existence puis de la validité du parametre en get
+if(isset($_GET["mail"])){
+	if(filter_var($_GET["mail"], FILTER_VALIDATE_EMAIL)){
+		//modification de la reponse si parametre valide
+    	$response = "ok";
+	}
+}
+
+return $response;
